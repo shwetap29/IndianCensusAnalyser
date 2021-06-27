51ceae37c24537a1bb6921affa9516619d7975e6
shwetap29 <shwetaprasad48@gmail.com>
2021-06-27 21:44:30 +0530

    ADDED:Used Iterator To Load The data

--- src/main/java/com/bridgelabz/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -1,7 +1,34 @@
 package com.bridgelabz;
 
+import com.bridgelabz.com.bridgelabz.CensusAnalyserException;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.stream.StreamSupport;
+
 public class CensusAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Census Analyser Problem");
+        public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+            try {
+                Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+                CsvToBeanBuilder<IndiaStateCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+                csvToBeanBuilder.withType(IndiaStateCensusCSV.class);
+                csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+                CsvToBean<IndiaStateCensusCSV> csvToBean = csvToBeanBuilder.build();
+                Iterator<IndiaStateCensusCSV> censusCSVIterator = csvToBean.iterator();
+                Iterable<IndiaStateCensusCSV> csvIterable = () -> censusCSVIterator;
+                return (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+            } catch (IOException e) {
+                throw new CensusAnalyserException(e.getMessage(),
+                        CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+            } catch (RuntimeException e) {
+                throw new CensusAnalyserException(e.getMessage(),
+                        CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+            }
+        }
     }
-}
+
--- src/main/java/com/bridgelabz/com/bridgelabz/CensusAnalyserException.java
+++ src/main/java/com/bridgelabz/com/bridgelabz/CensusAnalyserException.java
@@ -1,4 +1,20 @@
 package com.bridgelabz.com.bridgelabz;
 
-public class CensusAnalyserException {
-}
+public class CensusAnalyserException extends Throwable {
+
+    public enum ExceptionType {
+        CENSUS_FILE_PROBLEM, UNABLE_TO_PARSE
+
+    }
+     public ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+        super(message, cause);
+        this.type = type;
+    }
+    }
+
